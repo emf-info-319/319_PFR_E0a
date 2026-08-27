173d56d681124d577fb88e541afeea1519bbbbaa
Paul Friedli <paul.friedli@edufr.ch>
2026-08-27 17:41:44 +0200

    Initial commit

---
+++ Application.java
@@ -0,0 +1,36 @@
+public class Application {
+
+    // -----------------------------------------------------------
+    // Sujets : types, variables, tests, tests multiples et switch
+    // -----------------------------------------------------------
+
+    public static void main(String[] args) {
+
+        // Pour chaque type de base mis à disposition par Java :
+        // - déclarez une variable avec un nom explicite et clair
+        // - donnez-lui une valeur sensée
+        // - affichez cette variable sur la console avec le texte logique qui va avec
+        // (par exemple "Vous avez eu 12 retraits de permis !")
+
+        // Déclarez une variable qui puisse contenir le nom d'un module à l'EMF et
+        // donnez-lui une valeur, par exemple "319".
+
+        // Déclarez une variable qui puisse contenir une note modulaire et donnez-lui
+        // une valeur à choix entre 1 (compris) et 6 (compris), par exemple 2.
+
+        // Affichez "Le module 319 est réussi !" ou "Le module 319 n'est malheureusement
+        // pas réussi..." en fonction de votre variable qui contient une note modulaire
+
+        // Sur une même ligne de la console, affichez le texte
+        // "Au module 319 vous avez eu la note 2 qui veut dire 'Très insuffisant'".
+        // Pour rappel, voici les signification des notes :
+        // 1 => 'Pas rendu ou triché',
+        // 2 => 'Très insuffisant',
+        // 3 => 'Insuffisant',
+        // 4 => 'Suffisant',
+        // 5 => 'Bien',
+        // 6 => 'Très bien'
+
+    }
+
+}
